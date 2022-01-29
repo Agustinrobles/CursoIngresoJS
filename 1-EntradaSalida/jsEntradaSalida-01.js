66d15678bb865ce831d8d027a0c3f10341a07488
Robles Agustin <agustinrobles805@gmail.com>
2022-01-29 14:31:00 -0300

    nuevo cambio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("curso de iongreso UTNFRA 22");
+	alert("UTNFRA 2022 curso de ingreso");
 }
 
